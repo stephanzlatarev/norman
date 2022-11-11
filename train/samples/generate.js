ccd6eebf8bd85c200f452ffb3cdc1e6fd7c72b10
Stephan Zlatarev <stephan.zlatarev@sap.com>
2022-11-11 18:08:28 +0200

    Train samples generator improved

--- train/samples/generate.js
+++ train/samples/generate.js
@@ -2,8 +2,6 @@
 // Then use the pov function in the given game to transfer the scenario data into learning samples
 
 import fs from "fs";
-import { pov } from "../starcraft/game.js";
-import { toData } from "../starcraft/commands.js";
 
 // TODO: Use the gherkin skill file
 const SCENARIOS = [
@@ -25,56 +23,83 @@ const SCENARIOS = [
 export default async function() {
   const samples = [];
 
-  for (let wdx = 0; wdx <= 10; wdx += 0.5) {
-    for (let wdy = -10; wdy <= 10; wdy += 0.5) {
-      if ((wdx === 0) && (wdy === 0)) continue;
+  // Wing is straight ahead
+  for (let wingDistance = 1; wingDistance <= 8; wingDistance++) {
+    for (let enemyDistance = 1; enemyDistance <= 8; enemyDistance++) {
+      // Attack towards wing
+      addSample(samples, enemyDistance, wingDistance, 1, true, 1);
+    }
+  }
 
-      for (let edy = 1; edy <= 15; edy++) {
-        let shouldAttack = false;
+  // Wing is to the right and ahead
+  addSample(samples, 1, 1, 2, true, 1);
+  for (let enemyDistance = 2; enemyDistance <= 8; enemyDistance++) {
+    // Move forward surrounding enemy
+    addSample(samples, enemyDistance, 1, 2, false, 8);
+  }
 
-        if ((edy === 1) && (wdx === 0.5) && (wdy === 0.5)) shouldAttack = true; // We're in formation
-        if (wdy >= edy) shouldAttack = true; // We surround the enemy
+  // Wing is to the right and ahead but a distance
+  for (let wingDistance = 2; wingDistance <= 8; wingDistance++) {
+    // When enemy is close move back in wing's direction
+    addSample(samples, 1, wingDistance, 2, false, 3);
 
-        addSample(samples, { input: generateInput(wdx, wdy, edy), output: generateOutput(shouldAttack, wdx, wdy, edy) });
-      }
+    for (let enemyDistance = 2; enemyDistance <= 8; enemyDistance++) {
+      // Move towards enemy
+      addSample(samples, enemyDistance, wingDistance, 2, false, 1);
+    }
+  }
+
+  // Wing is right beside me
+  addSample(samples, 1, 1, 3, true, 1);
+  for (let enemyDistance = 2; enemyDistance <= 8; enemyDistance++) {
+    // Move forward surrounding enemy
+    addSample(samples, enemyDistance, 1, 3, false, 8);
+  }
+
+  // Wing is beside me but a distance
+  for (let wingDistance = 2; wingDistance <= 8; wingDistance++) {
+    // When enemy is close move back in wing's direction
+    addSample(samples, 1, wingDistance, 3, false, 4);
+
+    for (let enemyDistance = 2; enemyDistance <= 8; enemyDistance++) {
+      // Move towards wing
+      addSample(samples, enemyDistance, wingDistance, 3, false, 3);
+    }
+  }
+
+  // Wing is behind me
+  for (let wingDistance = 1; wingDistance <= 8; wingDistance++) {
+    for (let enemyDistance = 1; enemyDistance <= 8; enemyDistance++) {
+      // Move back towards wing
+      addSample(samples, enemyDistance, wingDistance, 4, false, 4);
+      addSample(samples, enemyDistance, wingDistance, 5, false, 5);
     }
   }
 
   fs.writeFileSync("./train/sandbox/samples/test.json", "[\r\n" + samples.join(",\r\n") + "\r\n]");
 }
 
-function addSample(samples, sample) {
-  const sampleAsText = JSON.stringify(sample);
-
-  if (samples.indexOf(sampleAsText) < 0) {
-    samples.push(sampleAsText);
-  }
+function addSample(samples, enemyDistance, wingDistance, wingAngle, commandAttack, commandAngle) {
+  samples.push(JSON.stringify({ input: generateInput(enemyDistance, wingDistance, wingAngle), output: generateOutput(commandAttack, commandAngle) }));
 }
 
-function generateInput(wingDistanceX, wingDistanceY, enemyDistanceY) {
-  const SELF_X = 100;
-  const SELF_Y = 100;
-  const situation = [
-    { tag: "self", owner: 1, pos: { x: SELF_X, y: SELF_Y } },
-    { tag: "wingman", owner: 1, pos: { x: SELF_X + wingDistanceX, y: SELF_Y + wingDistanceY } },
-    { tag: "enemy", owner: 2, pos: { x: SELF_X, y: SELF_Y + enemyDistanceY } },
-  ];
+const POV_SIZE = 65*2;
+function generateInput(enemyDistance, wingDistance, wingAngle) {
+  const pov = [];
+  for (let i = 0 ; i < POV_SIZE; i++) pov.push(0);
 
-  return pov(situation, "self");
+  // self
+  pov[65] = 0.1;
+
+  // wing
+  pov[65 + (wingDistance - 1) * 8 + wingAngle] = 0.1;
+
+  // enemy
+  pov[(enemyDistance - 1) * 8 + 1] = 0.1;
+
+  return pov;
 }
 
-function generateOutput(shouldAttack, wingDistanceX, wingDistanceY, enemyDistanceY) {
-  let output;
-
-  if (shouldAttack) {
-    output = toData({ abilityId: 3674, x: 0, y: enemyDistanceY });
-  } else {
-    output = toData({ abilityId: 16, x: wingDistanceX - 0.5, y: wingDistanceY - 0.5 }); // Get in formation relative to the enemy
-  }
-
-  if (output[0] === 0.15) {
-    output = toData({ abilityId: 3674, x: 0, y: enemyDistanceY });
-  }
-
-  return output;
+function generateOutput(commandAttack, commandAngle) {
+  return [commandAttack ? 0.85 : 0.5, (commandAngle - 1) / 8];
 }
