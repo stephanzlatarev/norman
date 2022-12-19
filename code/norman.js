54628f88d72bd8b08136d963774abb067ba98bd3
Stephan Zlatarev <stephan.zlatarev@sap.com>
2022-12-19 12:59:00 +0200

    Move norman's code to "code" folder

---
+++ code/norman.js
@@ -0,0 +1,40 @@
+import Body from "./body.js";
+import Goal from "./goal.js";
+import Memory from "./memory.js";
+
+export default class Norman {
+
+  constructor(env) {
+    this.memory = new Memory(env);
+    this.goal = new Goal(this.memory.get("goal"));
+    this.body = new Body(this.memory.get("body"));
+  }
+
+  async go() {
+    console.log("Hi!");
+
+    while (this.goal.ok() && this.body.ok()) {
+
+      // Bodies should now read the changes in their situation
+      await this.body.tick();
+
+      // Run all skills towards the goals
+      await this.goal.tick();
+
+      // Bodies should now execute the performed actions
+      await this.body.tock();
+
+      // Give space for asynchronous operations
+      await new Promise(r => setTimeout(r));
+    }
+
+    console.log("Bye!");
+  }
+  
+  stop() {
+    if (body.ok()) {
+      body.detach();
+    }
+  }
+
+}
--- norman.js
+++ norman.js
@@ -1,7 +1,5 @@
 import fs from "fs";
-import Body from "./body/body.js";
-import Goal from "./body/goal.js";
-import Memory from "./body/memory.js";
+import Norman from "./code/norman.js";
 
 const print = console.log;
 
@@ -9,41 +7,10 @@ console.log = function() {
   print(`[${new Date().toISOString()}]`, ...arguments);
 }
 
-const env = JSON.parse(fs.readFileSync("./norman.env").toString());
+const norman = new Norman(JSON.parse(fs.readFileSync("./norman.env").toString()));
 
-const memory = new Memory(env);
-const goal = new Goal(memory.get("goal"));
-const body = new Body(memory.get("body"));
+norman.go().catch(error => { console.log("ERROR:", error.message); console.log(error); });
 
-async function go() {
-  while (goal.ok() && body.ok()) {
-
-    // Bodies should now read the changes in their situation
-    await body.tick();
-
-    // Run all skills towards the goals
-    await goal.tick();
-
-    // Bodies should now execute the performed actions
-    await body.tock();
-
-    // Give space for asynchronous operations
-    await new Promise(r => setTimeout(r));
-  }
-
-  console.log("Bye!");
-}
-
-function stop() {
-  console.log("Stopping...");
-
-  if (body.ok()) {
-    body.detach();
-  }
-}
-
-go().catch(error => { console.log("ERROR:", error.message); console.log(error); });
-
-process.on('SIGINT', stop);
-process.on('SIGQUIT', stop);
-process.on('SIGTERM', stop);
+process.on('SIGINT', () => norman.stop());
+process.on('SIGQUIT', () => norman.stop());
+process.on('SIGTERM', () => norman.stop());
