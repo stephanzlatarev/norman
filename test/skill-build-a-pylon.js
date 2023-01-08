af1e9293269dcc9642845bf3ae1f1ca3f2e89146
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-01-08 16:38:00 +0200

    Update test for skill "build a pylon"

--- test/skill-build-a-pylon.js
+++ test/skill-build-a-pylon.js
@@ -29,24 +29,6 @@ describe("Skill 'build a pylon'", function() {
     ]);
   });
 
-  it("when the pylon is built", function() {
-    const memory = new Memory();
-    const game = memory.get("game").set("is-game", true);
-    const goalBuildPylon = memory.get("goal/build-a-pylon");
-    const probe = memory.get("body/probe-1").set("unitType", "probe").set("x", 101).set("y", 102);
-    const location = memory.get(goalBuildPylon.path + "/location").set("x", 201).set("y", 202);
-    const pylon1 = memory.get("body/pylon-1").set("unitType", "pylon").set("x", 201).set("y", 202);
-    const pylon2 = memory.get("body/pylon-2").set("unitType", "pylon").set("x", 301).set("y", 302);
-
-    goalBuildPylon.set("builder", probe);
-    goalBuildPylon.set("location", location);
-
-    assertEqual(memory, memory.layers(goalBuildPylon, game, PATTERN), [
-      { "PROBE": probe.path, "LOCATION": location.path, "PYLON": pylon1.path },
-      { "PROBE": probe.path, "LOCATION": location.path, "PYLON": pylon2.path },
-    ]);
-  });
-
 });
 
 function assertEqual(memory, actualLayers, expectedLayers) {
