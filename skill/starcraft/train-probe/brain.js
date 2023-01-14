8fcf80446d98efed83e17db69f824407d8c6c861
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-01-14 19:48:11 +0200

    Train probes when there's enough psi

--- skill/starcraft/train-probe/brain.js
+++ skill/starcraft/train-probe/brain.js
@@ -5,8 +5,10 @@ export default class Brain {
     const isOperational = input[0];
     const orders = input[1];
     const minerals = input[2];
+    const foodUsed = input[3];
+    const foodCap = input[4];
 
-    if (isOperational && (orders === 0) && (minerals >= 50)) {
+    if (isOperational && (orders === 0) && (minerals >= 50) && (foodUsed < foodCap)) {
       return [1, minerals - 50];
     }
   }
