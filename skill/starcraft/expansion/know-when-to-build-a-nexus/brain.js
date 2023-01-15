23584072c0354b395252094d366e2bedc66613b7
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-01-15 21:45:41 +0200

    Limit nexuses by number of pylons

--- skill/starcraft/expansion/know-when-to-build-a-nexus/brain.js
+++ skill/starcraft/expansion/know-when-to-build-a-nexus/brain.js
@@ -3,8 +3,10 @@ export default class KnowWhenToBuildANexusBrain {
 
   react(input) {
     const minerals = input[0];
+    const nexusCount = input[1];
+    const pylonCount = input[2];
 
-    if (minerals >= 300) {
+    if ((minerals >= 300) && (nexusCount <= pylonCount)) {
       return [1];
     }
   }
