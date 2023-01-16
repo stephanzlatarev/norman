6c19c4c8f453a747ea5a4b9cc1575334c6fa401f
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-01-16 22:58:05 +0200

    Scouting prefers locations far from the homebase

--- skill/starcraft/fighters/scout/brain.js
+++ skill/starcraft/fighters/scout/brain.js
@@ -2,29 +2,39 @@
 export default class Brain {
 
   react(input) {
-    const location = input[0];
-    const alternative = input[5] ? input[5] : input[1];
-    const thisZealot = input[2];
-    const partnerZealor = input[3];
-    const enemyCount = input[4];
+    const locationX = input[0];
+    const locationY = input[1];
+    const alternativeX = input[9] ? input[10] : input[2];
+    const alternativeY = input[9] ? input[11] : input[3];
+    const homebaseX = input[4];
+    const homebaseY = input[5];
+    const thisZealot = input[6];
+    const partnerZealot = input[7];
+    const enemyCount = input[8];
 
     if (enemyCount) {
       // There are enemies to be fought
-      if (location === alternative) {
-        return [-1];
+      if ((locationX === alternativeX) && (locationY === alternativeY)) {
+        return [-1, locationX, locationY];
       } else {
         return;
       }
     }
 
-    if (partnerZealor && (partnerZealor !== thisZealot)) {
+    if (partnerZealot && (partnerZealot !== thisZealot)) {
       // This location is already scouted
       return;
     }
 
-    if (!alternative) {
-      return [1];
+    if (!alternativeX && !alternativeY) {
+      return [1, locationX, locationY];
+    } else if (distance(locationX, locationY, homebaseX, homebaseY) > distance(alternativeX, alternativeY, homebaseX, homebaseY)) {
+      return [1, locationX, locationY];
     }
   }
 
+}
+
+function distance(x1, y1, x2, y2) {
+  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
 }
