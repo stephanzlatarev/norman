67b174b23d7370982e53453c2031b9fb767fe865
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-01-19 12:04:22 +0200

    Copy baseX and baseY from resource cluster to nexus

--- body/starcraft/structures.js
+++ body/starcraft/structures.js
@@ -35,27 +35,6 @@ function near(structure, x, y) {
 
 export function linkNexusToCluster(nexus, cluster) {
   nexus.set("resources", cluster);
-
-  const a = angle(nexus.get("x") - cluster.get("x"), nexus.get("y") - cluster.get("y"));
-  nexus.set("faceSector", a[0]);
-  nexus.set("faceSide", a[1]);
-}
-
-function angle(dx, dy) {
-  if (!dx && !dy) return [0, 0];
-
-  const d = (Math.abs((Math.abs(dx) - Math.abs(dy)) / (Math.abs(dx) + Math.abs(dy))) > 0.4142) ? 1 : -1;
-
-  let a = 0;
-  if ((dx >= 0) && (dy >= 0)) {
-    a = (dx > dy) ? 1 : 0;
-  } else if ((dx >= 0) && (dy < 0)) {
-    a = (dx > -dy) ? 2 : 3;
-  } else if ((dx < 0) && (dy < 0)) {
-    a = (-dx > -dy) ? 5 : 4;
-  } else if ((dx < 0) && (dy >= 0)) {
-    a = (-dx > dy) ? 6 : 7;
-  }
-
-  return [a, d];
+  nexus.set("baseX", cluster.get("baseX"));
+  nexus.set("baseY", cluster.get("baseY"));
 }
