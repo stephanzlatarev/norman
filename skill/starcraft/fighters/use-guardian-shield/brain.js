245cc214d01d67885eb7bfe06f3ae9bf0a361fe5
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-01-24 15:00:34 +0200

    sentry uses guardian shield only when close to enemy

--- skill/starcraft/fighters/use-guardian-shield/brain.js
+++ skill/starcraft/fighters/use-guardian-shield/brain.js
@@ -3,11 +3,28 @@ export default class Brain {
 
   react(input) {
     const energy = input[0];
-    const alreadySelectedSentry = input[1];
+    const sentryX = input[1];
+    const sentryY = input[2];
+    const enemyX = input[3];
+    const enemyY = input[4];
 
-    if (!alreadySelectedSentry && (energy >= 75)) {
-      return [1, 1];
+    const guardian = input[5];
+    const guardianX = input[6];
+    const guardianY = input[7];
+
+    if (enemyX && enemyY && (energy >= 75)) {
+      const distanceToEnemy = distance(sentryX, sentryY, enemyX, enemyY);
+
+      if (distanceToEnemy <= 100) {
+        if (!guardian || (distanceToEnemy < distance(sentryX, sentryY, guardianX, guardianY))) {
+          return [1, sentryX, sentryY];
+        }
+      }
     }
   }
 
+}
+
+function distance(x1, y1, x2, y2) {
+  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
 }
