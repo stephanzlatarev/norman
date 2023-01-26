8de997e411bfc6e2a233c56d8dbcc2809909ecb4
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-01-26 20:03:34 +0200

    Fix gateway build skills

--- body/starcraft/unit/gateway/body.js
+++ body/starcraft/unit/gateway/body.js
@@ -9,10 +9,13 @@ export default class Gateway extends Unit {
   async tock() {
     if (this.node.get("build-zealot")) {
       super.command(916, null, null, "build-zealot");
+      this.node.clear("build-zealot");
     } else if (this.node.get("build-stalker")) {
       super.command(917, null, null, "build-stalker");
+      this.node.clear("build-stalker");
     } else if (this.node.get("build-sentry")) {
       super.command(921, null, null, "build-sentry");
+      this.node.clear("build-sentry");
     }
 
     await super.tock();
