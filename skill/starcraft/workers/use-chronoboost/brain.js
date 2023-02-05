89b74d2bff72de46db6e055785d75a892ef19d2d
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-02-05 20:11:22 +0200

    Remove use chronoboost on gateway

--- skill/starcraft/workers/use-chronoboost/brain.js
+++ skill/starcraft/workers/use-chronoboost/brain.js
@@ -4,9 +4,8 @@ export default class Brain {
   react(input) {
     const nexusIsOperational = input[0];
     const nexusEnergy = input[1];
-    const nexusGateways = input[2];
 
-    if (nexusIsOperational && !nexusGateways && (nexusEnergy >= 50)) {
+    if (nexusIsOperational && (nexusEnergy >= 50)) {
       return [1, nexusEnergy - 50];
     }
   }
