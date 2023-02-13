0439a9a8761e43f48977abd82fe67dc296be0c33
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-02-13 13:10:41 +0200

    Skill to select location for pylons won't power new bases in single base

--- skill/starcraft/pylons/select-location/brain.js
+++ skill/starcraft/pylons/select-location/brain.js
@@ -7,14 +7,14 @@ const LOCATIONS = [
 export default class Brain {
 
   react(input) {
-    const powerNewBase = input[0];
-    const baseX = input[1];
-    const baseY = input[2];
-    const distance = input[3];
-    const pylons = input[4];
+    const powerNewBase = input[1] && (input[0] !== 1);
+    const baseX = input[2];
+    const baseY = input[3];
+    const distance = input[4];
+    const pylons = input[5];
 
-    const selectedLocation = input[5];
-    const distanceOfSelectedLocation = input[8];
+    const selectedLocation = input[6];
+    const distanceOfSelectedLocation = input[9];
 
     if ((powerNewBase && pylons) || (pylons >= LOCATIONS.length)) {
       // The base of this nexus is already full of pylons, or we need to power a new base
