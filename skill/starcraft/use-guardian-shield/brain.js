b163b4332e840f3e396040c48565cb3e80a5d01d
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-06-23 20:25:13 +0300

    Restore skill use-guardian-shield

--- skill/starcraft/use-guardian-shield/brain.js
+++ skill/starcraft/use-guardian-shield/brain.js
@@ -1,20 +1,10 @@
 
 export default class Brain {
 
-  react(energy, sentryX, sentryY, enemies, enemyX, enemyY, guardianDistance) {
-    if ((enemies > 2) && enemyX && enemyY && (energy >= 75)) {
-      const distanceToEnemy = distance(sentryX, sentryY, enemyX, enemyY);
-
-      if (distanceToEnemy <= 100) {
-        if (!guardianDistance || (distanceToEnemy < guardianDistance)) {
-          return [1, distanceToEnemy];
-        }
-      }
+  react(abilityId, energy) {
+    if ((abilityId === 23) && (energy >= 75)) {
+      return [1];
     }
   }
 
-}
-
-function distance(x1, y1, x2, y2) {
-  return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
 }
