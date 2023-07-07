900e0d58f4b16f81c27bc4f4a1999c97c8906679
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-07-07 09:44:08 +0300

    Add ping.bat to tool/train

--- tool/train/ping.bat
+++ tool/train/ping.bat
@@ -6,9 +6,10 @@ echo Trainer 1
 call k norman log trainer-1 trainer --tail=3
 echo Trainer 2
 call k norman log trainer-2 trainer --tail=3
-echo Trainer 3
-call k norman log trainer-3 trainer --tail=3
-echo Trainer 4
-call k norman log trainer-4 trainer --tail=3
-echo Trainer 5
-call k norman log trainer-5 trainer --tail=3
+
+echo Trainer 0 (slim)
+call k norman -n=slim log trainer-0 trainer --tail=3
+echo Trainer 1 (slim)
+call k norman -n=slim log trainer-1 trainer --tail=3
+echo Trainer 2 (slim)
+call k norman -n=slim log trainer-2 trainer --tail=3
