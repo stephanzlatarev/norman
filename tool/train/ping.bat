e45a4ab7da3789a685398d93f84f8f4698e596ca
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-08-11 16:26:09 +0300

    tool/train splits error stats by playbook

--- tool/train/ping.bat
+++ tool/train/ping.bat
@@ -1,8 +1,8 @@
 echo off
 
 echo Trainer 0
-call k norman log trainer-0 trainer --tail=3
+call k norman log trainer-0 trainer --tail=8
 echo Trainer 1
-call k norman log trainer-1 trainer --tail=3
+call k norman log trainer-1 trainer --tail=8
 echo Trainer 2
-call k norman log trainer-2 trainer --tail=3
+call k norman log trainer-2 trainer --tail=8
--- tool/train/train.js
+++ tool/train/train.js
@@ -1,12 +1,8 @@
 import fs from "fs";
 import * as tf from "@tensorflow/tfjs-node";
 
-// TODO: Load all javascript files with name starting with "playbook-"
-import playbook1 from "./skill/playbook-initial-rush.js";
-import playbook2 from "./skill/playbook-create-frontline.js";
-
 const BRAIN = "/data/brain.tf";
-const PLAYBOOKS = [playbook1, playbook2];
+const PLAYBOOKS = "./skill/playbook/";
 
 const SAMPLES_COUNT = 5000;
 const INPUT_SIZE = 400;
@@ -18,13 +14,30 @@ const OUTPUT_ACTIVATION_FUNCTION = "sigmoid";
 const OPTIMIZER_FUNCTION = "adam";
 const LOSS_FUNCTION = "meanSquaredError";
 
-function generateSamples() {
+async function loadPlaybooks() {
+  const scripts = fs.readdirSync(PLAYBOOKS).filter(name => name.endsWith(".js"));
+  const playbooks = [];
+
+  for (const script of scripts) {
+    const module = await import(PLAYBOOKS + script);
+    playbooks.push({
+      name: script.substring(0, script.length - 3),
+      sample: module.default,
+    });
+  }
+
+  return playbooks;
+}
+
+function generateSamples(playbooks) {
+  const source = [];
   const input = [];
   const output = [];
 
-  for (const playbook of PLAYBOOKS) {
+  for (const playbook of playbooks) {
     for (let i = 0; i < SAMPLES_COUNT; i++) {
-      const sample = playbook();
+      const sample = playbook.sample();
+      source.push(playbook.name);
       input.push(sample.input);
       output.push(sample.output);
     }
@@ -32,6 +45,7 @@ function generateSamples() {
 
   return {
     length: input.length,
+    source: source,
     input: input,
     inputSize: INPUT_SIZE,
     output: output,
@@ -39,10 +53,10 @@ function generateSamples() {
   };
 }
 
-async function run(model) {
+async function run(playbooks, model) {
   tf.engine().startScope();
 
-  const samples = generateSamples();
+  const samples = generateSamples(playbooks);
   const input = tf.tensor(samples.input, [samples.input.length, samples.inputSize]);
   const output = tf.tensor(samples.output, [samples.output.length, samples.outputSize]);
   const info = await model.fit(input, output, { epochs: LEARNING_EPOCHS, batchSize: LEARNING_BATCH, shuffle: true, verbose: false });
@@ -50,7 +64,7 @@ async function run(model) {
 
   const result = {
     loss: info.history.loss.reduce((best, current) => Math.min(current, best), Infinity),
-    error: error(samples.output, predictions),
+    error: error(samples, predictions),
   };
 
   tf.engine().endScope();
@@ -58,40 +72,49 @@ async function run(model) {
   return result;
 }
 
-function error(output, predictions) {
-  let errors = [];
+function error(samples, predictions) {
+  const errors = { total: [] };
+  const result = {};
 
   for (let i = 0; i < predictions.length; i++) {
     let error = 0;
 
     for (let j = 0; j < predictions[i].length; j++) {
-      error = Math.max(error, Math.abs(output[i][j] - predictions[i][j]));
+      error = Math.max(error, Math.abs(samples.output[i][j] - predictions[i][j]));
     }
 
-    errors.push(error);
+    errors.total.push(error);
+    if (!errors[samples.source[i]]) errors[samples.source[i]] = [];
+    errors[samples.source[i]].push(error);
   }
-  errors.sort((a, b) => (a - b));
 
-  let pass = 0;
-  let fail = 0;
-  for (const error of errors) {
-    if (error < 0.01) {
-      pass++;
-    } else if (error > 0.99) {
-      fail++;
+  for (const key in errors) {
+    const errs = errors[key];
+    errs.sort((a, b) => (a - b));
+
+    let pass = 0;
+    let fail = 0;
+    for (const error of errs) {
+      if (error < 0.01) {
+        pass++;
+      } else if (error > 0.99) {
+        fail++;
+      }
     }
+
+    result[key] = {
+      pass: pass / errs.length,
+      fail: fail,
+      best: errs[0],
+      a: errs[Math.floor(errs.length * 0.9)],
+      b: errs[Math.floor(errs.length * 0.99)],
+      c: errs[Math.floor(errs.length * 0.999)],
+      d: errs[Math.floor(errs.length * 0.9999)],
+      worst: errs[errs.length - 1],
+    };
   }
 
-  return {
-    pass: pass / errors.length,
-    fail: fail,
-    best: errors[0],
-    a: errors[Math.floor(errors.length * 0.9)],
-    b: errors[Math.floor(errors.length * 0.99)],
-    c: errors[Math.floor(errors.length * 0.999)],
-    d: errors[Math.floor(errors.length * 0.9999)],
-    worst: errors[errors.length - 1],
-  };
+  return result;
 }
 
 function create() {
@@ -124,32 +147,37 @@ async function save(model, file) {
   });
 }
 
-function show(iteration, result) {
-  const line = [new Date().toISOString().split("T")[1].split(".")[0], iteration];
+function show(iteration, results) {
+  console.log(new Date().toISOString().split("T")[1].split(".")[0], "iteration:", iteration, "loss:", results.loss);
 
-  const error = result.error;
-  line.push("best: " + error.best.toFixed(5));
-  line.push(error.a.toFixed(2));
-  line.push(error.b.toFixed(2));
-  line.push(error.c.toFixed(2));
-  line.push(error.d.toFixed(2));
-  line.push("worst: " + error.worst.toFixed(5));
-  line.push("pass: " + (error.pass * 100).toFixed(2) + "%");
-  line.push("fail: " + error.fail);
+  for (const key in results.error) {
+    const line = [""];
+    const error = results.error[key];
 
-  line.push("loss: " + result.loss);
+    line.push("best: " + error.best.toFixed(5));
+    line.push(error.a.toFixed(2));
+    line.push(error.b.toFixed(2));
+    line.push(error.c.toFixed(2));
+    line.push(error.d.toFixed(2));
+    line.push("worst: " + error.worst.toFixed(5));
+    line.push("pass: " + (error.pass * 100).toFixed(2) + "%");
+    line.push("fail: " + error.fail);
 
-  console.log(line.join("\t"));
+    line.push(key);
+
+    console.log(line.join("\t"));
+  }
 }
 
 async function train() {
+  const playbooks = await loadPlaybooks();
   const model = fs.existsSync(BRAIN) ? await load(BRAIN) : create();
 
   model.summary();
 
   let iteration = 0;
   while (++iteration > 0) {
-    const attempt = await run(model);
+    const attempt = await run(playbooks, model);
 
     show(iteration, attempt);
 
