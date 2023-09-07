32c21f860976ed5d19eebce3d345ca662824fa0c
Stephan Zlatarev <stephan.zlatarev@sap.com>
2023-09-07 15:38:21 +0300

    Remove tool folder. Moved to norman-gym

--- tool/train/deploy.bat
+++
@@ -1,4 +0,0 @@
-
-docker build -t docker.io/stephanzlatarev/norman-trainer .
-docker push docker.io/stephanzlatarev/norman-trainer
-call k norman apply -f ./kubernetes.yaml
--- tool/train/ping.bat
+++
@@ -1,8 +0,0 @@
-echo off
-
-echo Trainer 0
-call k norman log trainer-0 trainer --tail=8
-echo Trainer 1
-call k norman log trainer-1 trainer --tail=8
-echo Trainer 2
-call k norman log trainer-2 trainer --tail=8
