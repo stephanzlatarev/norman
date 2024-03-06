dd4cac9728cbabf48b5d3c82d5b82d80fb983c59
Stephan Zlatarev <stephan.zlatarev@sap.com>
2024-03-06 21:14:10 +0200

    Remove old mind skills. They will be replaced with body skills.

--- skill/starcraft/plan-investments/strategy-zealots-only.js
+++
@@ -1,9 +0,0 @@
-import Strategy from "./strategy.js";
-
-export default class ZealotsOnly extends Strategy {
-
-  units() {
-    return ["zealots"];
-  }
-
-}
--- skill/starcraft/produce-unit/brain.js
+++
@@ -1,9 +0,0 @@
-
-export default class Brain {
-
-  react() {
-    // Issue command and remove the goal
-    return [1, 0];
-  }
-
-}
