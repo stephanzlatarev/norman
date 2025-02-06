56b961169750d5b794fc9a18c560c5e4a2f0aa45
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2025-02-06 17:16:59 +0200

    Invisible threats are ignored only when we're not on one base

--- body/starcraft/battle/update-threats.js
+++ body/starcraft/battle/update-threats.js
@@ -1,6 +1,9 @@
 import Battle from "./battle.js";
+import Plan from "../memo/plan.js";
 
 export default function(battle) {
+  if (Plan.BaseLimit) return;
+
   const isAssault = (battle.mode === Battle.MODE_FIGHT) || (battle.mode === Battle.MODE_SMASH);
   const hasDetector = battle.detector && battle.detector.assignee;
 
@@ -12,10 +15,13 @@ export default function(battle) {
 function ignoreInvisibleThreats(zone) {
   const threats = zone.threats;
   const visible = zone.enemies;
+  const count = threats.size;
 
   for (const threat of threats) {
     if (!visible.has(threat)) {
       threats.delete(threat);
     }
   }
+
+  if (threats.size !== count) console.log("Zone", zone.name, "cleared of invisible threats");
 }
