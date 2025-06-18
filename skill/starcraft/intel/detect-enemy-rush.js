c0e874888f7939378134c8620def8d24bb2be967
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2025-06-18 16:16:29 +0300

    Update enemy rush detection

--- skill/starcraft/intel/detect-enemy-rush.js
+++ skill/starcraft/intel/detect-enemy-rush.js
@@ -1,19 +1,9 @@
-import { Memory, Resources } from "./imports.js";
-
-const LOOPS_PER_SECOND = 22.4;
-const LOOPS_PER_MINUTE = LOOPS_PER_SECOND * 60;
-const LOOPS_FIVE_MINUTES = LOOPS_PER_MINUTE * 5;
+import { Memory } from "./imports.js";
 
 export default function() {
-  if (Memory.MilestoneBasicMilitary && !Memory.ExpectEnemyRush) return;
-
-  const previousExpectEnemyRush = Memory.ExpectEnemyRush;
   let expectEnemyRush = false;
 
-  if (Resources.loop > LOOPS_FIVE_MINUTES) {
-    // Don't expect enemy rush after the first 5 minutes
-    expectEnemyRush = false;
-  } else if (Memory.MilestoneMaxArmy) {
+  if (Memory.FlagSiegeDefense || Memory.MilestoneMaxArmy) {
     expectEnemyRush = false;
   } else if (Memory.DetectedEnemyProxy || Memory.DetectedEnemyHoard) {
     expectEnemyRush = true;
@@ -24,7 +14,7 @@ export default function() {
     expectEnemyRush = true;
   }
 
-  if (expectEnemyRush != previousExpectEnemyRush) {
+  if (expectEnemyRush != Memory.ExpectEnemyRush) {
     console.log(expectEnemyRush ? "Expect" : "Don't expect", "enemy rush");
     Memory.ExpectEnemyRush = expectEnemyRush;
   }
