a0f60acdf4f66a499f49bef3e9026fcc33570fae
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2025-10-26 14:54:37 +0200

    Improve milestone-max-army

--- skill/starcraft/introspection/milestone-max-army.js
+++ skill/starcraft/introspection/milestone-max-army.js
@@ -1,10 +1,16 @@
 import { Memory, Resources } from "./imports.js";
 
+let milestoneReached = false;
+
 export default function() {
-  if (Memory.MilestoneMaxArmy) return;
+  // TODO: Check if total units (active and in production) is close to max army size
+  // When we are rotating the army to stronger units, then we still are maxed out
+  if (Memory.MilestoneMaxArmy && (Resources.supplyUsed > 150)) return;
 
   if (Resources.supplyUsed < 196) return;
 
-  console.log("Milestone Max Army");
+  if (milestoneReached) console.log("Milestone Max Army");
+
   Memory.MilestoneMaxArmy = true;
+  milestoneReached = true;
 }
