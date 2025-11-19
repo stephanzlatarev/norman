e37d21ed902891f0101a312dba626bbb07160b88
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2025-11-19 15:00:16 +0200

    Remove old game-local tracing

--- body/starcraft/game-local.js
+++ body/starcraft/game-local.js
@@ -1,59 +1,9 @@
 import { spawn } from "child_process";
-import fs from "fs";
 import readline from "readline";
 import Game from "./game.js";
-import Trace from "./trace.js";
-
-const SIMULATION_FILE = "./body/starcraft/simulation.js";
-const SIMULATION_CODE = `
-let done = false;
-
-export default async function simulate(client) {
-  if (done) return;
-
-  const gameInfo = await client.gameInfo();
-  const observation = await client.observation();
-  const base = gameInfo.startRaw.startLocations[0];
-
-  if (meta.map !== gameInfo.mapName) {
-    console.log("The simulation must run on map ", meta.map);
-    process.exit();
-  }
-  if ((meta.x !== base.x) || (meta.x !== base.x)) {
-    console.log("The simulation must run with the other start location! Start location is random. Try it again.");
-    process.exit();
-  }
-
-  const extras = observation.observation.rawData.units.filter(function(unit) {
-    if ((unit.owner !== 1) && (unit.owner !== 2)) return false;
-    return !units.find(one => ((unit.pos.x === one.x) && (unit.pos.y === one.y)));
-  });
-  await client.debug({ debug: [{ killUnit: { tag: extras.map(unit => Number(unit.tag)) } }] });
-
-  for (const unit of units) {
-    if (observation.observation.rawData.units.find(one => ((one.pos.x === unit.x) && (one.pos.y === unit.y)))) continue;
-
-    await client.debug({
-      debug: [{
-        createUnit: {
-          unitType: unit.type,
-          owner: unit.owner,
-          pos: { x: unit.x, y: unit.y },
-          quantity: 1,
-        }
-      }]
-    });
-  }
-
-  await client.step(1);
-
-  done = true;
-}
-`;
 
 let speed = 0;
 let paused = false;
-let takingSnapshot = false;
 
 export default class LocalGame extends Game {
 
@@ -61,7 +11,6 @@ export default class LocalGame extends Game {
     super();
 
     this.config = config;
-    this.trace = new Trace();
   }
 
   async connect() {
@@ -96,28 +45,10 @@ export default class LocalGame extends Game {
       options: { raw: true, score: true },
     });
 
-    if (fs.existsSync(SIMULATION_FILE)) {
-      const module = await import("../../code/simulation.js");
-      this.simulation = module.default;
-
-      console.log("Creating simulation...");
-      await this.simulation(this.client);
-      console.log("Simulation ready.");
-
-      paused = true;
-      console.log("Game paused!");
-    }
-
     console.log("Ready to play.");
   }
 
   async step() {
-    if (takingSnapshot) {
-      await save(this.client);
-      takingSnapshot = false;
-    }
-
-    await this.trace.step(this.client);
     await super.step();
 
     if (paused) {
@@ -130,10 +61,6 @@ export default class LocalGame extends Game {
       });
     } else if (speed) {
       await new Promise(resolve => setTimeout(resolve, speed));
-    }
-
-    if (this.simulation) {
-      await this.simulation(this.client);
     }
   }
 
@@ -159,61 +86,16 @@ function slow(value) {
   }
 }
 
-async function save(client) {
-  const gameInfo = await client.gameInfo();
-  const base = gameInfo.startRaw.startLocations[0];
-  const meta = `const meta = { map: "${gameInfo.mapName}", x: ${base.x}, y: ${base.y} };`;
-  const existing = new Set();
-
-  for (const unit of (await client.observation()).observation.rawData.units) {
-    existing.add(unit.tag);
-  }
-
-  for (let x = gameInfo.startRaw.playableArea.p0.x; x <= gameInfo.startRaw.playableArea.p1.x; x += 5) {
-    const addObservers = [];
-    for (let y = gameInfo.startRaw.playableArea.p0.y; y <= gameInfo.startRaw.playableArea.p1.y; y += 5) {
-      addObservers.push({ createUnit: { unitType: 82, owner: 1, pos: { x: x, y: y }, quantity: 1 } });
-    }
-    await client.debug({ debug: addObservers });
-  }
-
-  await client.step(1);
-  await client.step(1);
-  await client.step(1);
-
-  const observation = await client.observation();
-  const units = ["const units = ["];
-  for (const unit of observation.observation.rawData.units) {
-    if (((unit.owner === 1) && existing.has(unit.tag)) || (unit.owner === 2)) {
-      units.push(`  { owner: ${unit.owner}, type: ${unit.unitType}, x: ${unit.pos.x}, y: ${unit.pos.y} },`);
-    }
-  }
-  units.push("];");
-
-  const removeObservers = [];
-  for (const unit of observation.observation.rawData.units) {
-    if ((unit.unitType === 82) && !existing.has(unit.tag)) {
-      removeObservers.push(Number(unit.tag));
-    }
-  }
-  await client.debug({ debug: [{ killUnit: { tag: removeObservers } }] });
-
-  fs.writeFileSync(SIMULATION_FILE, meta + "\r\n" + units.join("\r\n") + SIMULATION_CODE);
-  console.log("Snapshot saved!");
-}
-
 readline.emitKeypressEvents(process.stdin);
 
 if (process.stdin.isTTY) process.stdin.setRawMode(true);
 
 console.log("Press 1-9 to control speed of game and 0 to resume normal speed");
 console.log("Press Space to pause game");
-console.log("Press S to save a simulation snapshot");
 console.log("Press Escape to exit");
 process.stdin.on("keypress", (chunk, key) => {
   if (key.name === "escape") process.exit(0);
   if (key.ctrl && (key.name == "c" || key.name == "x" || key.name == "C" || key.name == "X")) process.exit(0);
-  if (chunk == "s" || chunk == "S") takingSnapshot = true;
   if (chunk == " ") pause();
   if (chunk >= "0" && chunk <= "9") slow(chunk);
 });
