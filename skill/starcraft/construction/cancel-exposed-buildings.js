cf36d69ca1541b541fc424455f752426f47902d3
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2026-02-23 17:36:48 +0200

    Add cancel-exposed-buildings skill

---
+++ skill/starcraft/construction/cancel-exposed-buildings.js
@@ -0,0 +1,17 @@
+import { Depot, Memory, Order } from "./imports.js";
+
+export default function() {
+  if (Memory.DeploymentOutreach !== Memory.DeploymentOutreachSiegeDefense) return;
+  if (Memory.LevelEnemyArmySuperiority < 2) return;
+
+  for (const zone of Depot.list()) {
+    if (zone === Depot.home) continue;
+    if (zone.warriors.size > zone.enemies.size) continue;
+
+    for (const building of zone.buildings) {
+      if (building.buildProgress < 1) {
+        new Order(building, 3659).accept(true);
+      }
+    }
+  }
+}
--- skill/starcraft/plans/limit-bases.js
+++ skill/starcraft/plans/limit-bases.js
@@ -10,20 +10,3 @@ export default function() {
     Memory.LimitBase = Depot.list().length;
   }
 }
-
-// function cancelExposedConstructions() {
-//   for (const building of Units.buildings().values()) {
-//     // Keep all buildings in the home base
-//     if (building.zone === Depot.home) continue;
-
-//     // Keep the Gateway so that in case it finishes we can start a CyberneticsCore sooner
-//     if (building.type.name === "Gateway") continue;
-
-//     // Keep CyberneticsCore if it's halfway ready.
-//     if ((building.type.name === "CyberneticsCore") && (building.buildProgress > 0.5)) continue;
-
-//     if (building.buildProgress < 1) {
-//       new Order(building, 3659).accept(true);
-//     }
-//   }
-// }
