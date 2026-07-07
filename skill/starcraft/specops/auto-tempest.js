206a275606a004a1906ae7a62428fbc582efb4f2
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2026-07-07 11:13:07 +0300

    Improve auto-tempest commands

--- skill/starcraft/specops/auto-tempest.js
+++ skill/starcraft/specops/auto-tempest.js
@@ -1,26 +1,74 @@
 import { ActiveCount, Depot, Order, Resources, Units, Zone } from "./imports.js";
 
+const SQUARE_RANGE = 81; // This is optimized for a Queen target
+
 let target = null;
 
+// All Tempest units move in a clump with synchronized weapons
 export default function() {
   if (ActiveCount.Tempest === 0) return;
 
-  if (!target || !target.isValidShootingTarget()) target = findClosestEnemy();
-  if (!target) return;
+  const tempests = getTempests();
+  const center = calculateCenter(tempests);
 
-  for (const unit of Units.warriors().values()) {
-    if (unit.type.name === "Tempest") controlTempest(unit);
+  target = updateTarget(center);
+
+  const command = calculateCommand(center);
+
+  for (const tempest of tempests) {
+    if (command.action === "attack") {
+      Order.attack(tempest, command.target);
+    } else {
+      Order.move(tempest, command.target);
+    }
   }
 }
 
-function controlTempest(tempest) {
-  if ((tempest.weapon.cooldown > 3) && (tempest.weapon.cooldown < tempest.type.weaponCooldown - 3)) {
-    Order.move(tempest, Depot.home);
-  } else if (!tempest.isTargetInFireRange(target)) {
-    Order.move(tempest, target.body);
-  } else {
-    Order.attack(tempest, target);
+function getTempests() {
+  const tempests = [];
+
+  for (const unit of Units.warriors().values()) {
+    if (unit.type.name === "Tempest") {
+      tempests.push(unit);
+    }
   }
+
+  return tempests;
+}
+
+function updateTarget(pos) {
+  const closestEnemy = findClosestEnemy();
+
+  if (!closestEnemy) return;
+  const distanceClosestEnemy = calculateSquaredDistance(pos, closestEnemy.body);
+  if ((distanceClosestEnemy < SQUARE_RANGE) && !closestEnemy.isValidShootingTarget(true)) {
+    closestEnemy.sector.untrackUnit(closestEnemy);
+    return;
+  }
+
+  if (!target) return closestEnemy;
+  const distanceTarget = calculateSquaredDistance(pos, target.body);
+  if ((distanceTarget < SQUARE_RANGE) && !target.isValidShootingTarget(true)) {
+    target.sector.untrackUnit(target);
+    return closestEnemy;
+  }
+
+  if (target === closestEnemy) return closestEnemy;
+  if (target.zone !== closestEnemy.zone) return closestEnemy;
+  if (!target.isValidShootingTarget()) return closestEnemy;
+
+  return (distanceTarget <= distanceClosestEnemy) ? target : closestEnemy;
+}
+
+function calculateCommand(center) {
+  if (!target) return { action: "move", target: Depot.home };
+
+  const squareDistance = calculateSquaredDistance(center, target.body);
+
+  if (squareDistance < SQUARE_RANGE) return { action: "move", target: Depot.home };
+  if (!target.isValidShootingTarget(true)) return { action: "move", target: target.body };
+
+  return { action: "attack", target };
 }
 
 function findClosestEnemy() {
@@ -31,4 +79,28 @@ function findClosestEnemy() {
       }
     }
   }
+}
+
+function calculateCenter(units) {
+  let x = 0;
+  let y = 0;
+  let count = 0;
+
+  for (const unit of units) {
+    x += unit.body.x;
+    y += unit.body.y;
+    count++;
+  }
+
+  return {
+    x: x / count,
+    y: y / count,
+  };
+}
+
+function calculateSquaredDistance(a, b) {
+  const dx = a.x - b.x;
+  const dy = a.y - b.y;
+
+  return (dx * dx) + (dy * dy);
 }
