b6c39f0052b95b97630285d190b71e0b9f530580
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2026-07-17 12:30:32 +0300

    Reduce fighter jobs when necessary in combat skill

--- skill/starcraft/combat/update-open-jobs.js
+++ skill/starcraft/combat/update-open-jobs.js
@@ -1,16 +1,16 @@
 import { ActiveCount } from "./imports.js";
 import Fight from "./job-fight.js";
 
-const RECRUIT_BALANCE = 2;
-const CAP_BALANCE = 10;
+const MIN_FIGHTERS = 3;
+const MIN_RECRUIT_BALANCE = 2;
+const MAX_RECRUIT_BALANCE = 4;
+
 const ALL_WARRIORS = ["Colossus", "Immortal", "Sentry", "Stalker", "Zealot"];
 const GROUND_HITTING_WARRIORS = ["Colossus", "Immortal", "Zealot"];
 
 export default function(battle) {
-  const isBalanceInsufficient = (battle.recruitedBalance < RECRUIT_BALANCE);
-
-  if (battle.isFocusBattle || isBalanceInsufficient) {
-
+  if (battle.isFocusBattle || (battle.recruitedBalance < MIN_RECRUIT_BALANCE)) {
+    // Open new jobs
     closeOpenJobsOutsideBattle(battle);
 
     if (battle.isOnlyBattle) {
@@ -18,12 +18,12 @@ export default function(battle) {
       openJobs(battle, ...ALL_WARRIORS);
     } else if (battle.isSmallBattle) {
       // Make sure we don't overreact to individual enemy units in our territory
-      if (isBalanceInsufficient || (battle.fighters.length < 3)) {
+      if ((battle.recruitedBalance < MIN_RECRUIT_BALANCE) || (battle.fighters.length < MIN_FIGHTERS)) {
         openJobs(battle, "Stalker", "Zealot");
       }
 
       closeOpenJobs(battle, "Colossus", "Immortal", "Sentry");
-    } else if (battle.recruitedBalance < CAP_BALANCE) {
+    } else if ((battle.recruitedBalance < MAX_RECRUIT_BALANCE)) {
       openJobs(battle, "Sentry", "Stalker");
 
       // Make sure ground-hitting units are included only when there are ground enemy units
@@ -33,7 +33,13 @@ export default function(battle) {
         openJobs(battle, ...GROUND_HITTING_WARRIORS);
       }
     }
+  } else if (battle.recruitedBalance > MAX_RECRUIT_BALANCE) {
+    // Reduce jobs
+    closeOpenJobs(battle, ...ALL_WARRIORS);
+    if (battle.isAirBattle) closeJobs(battle, ...GROUND_HITTING_WARRIORS);
+    reduceJobs(battle, MIN_FIGHTERS);
   } else {
+    // Stop hiring
     closeOpenJobs(battle, ...ALL_WARRIORS);
   }
 }
@@ -56,6 +62,14 @@ function openJob(battle, warrior) {
   }
 }
 
+function closeJobs(battle, ...warriors) {
+  for (const job of battle.fighters) {
+    if (job.agent && (warriors.indexOf(job.agent.type.name) >= 0)) {
+      job.close(true);
+    }
+  }
+}
+
 function closeOpenJobs(battle, ...warriors) {
   for (const job of battle.fighters) {
     if (isJobOpen(job) && job.agent && (warriors.indexOf(job.agent.type.name) >= 0)) {
@@ -71,5 +85,11 @@ function closeOpenJobsOutsideBattle(battle) {
     if (job.zone !== battle.rally) {
       job.close(true);
     }
+  }
+}
+
+function reduceJobs(battle, limit) {
+  for (let i = limit; i < battle.fighters.length; i++) {
+    battle.fighters[i].close(true);
   }
 }
