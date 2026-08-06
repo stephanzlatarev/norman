06f847a59a2999f30dba1620a485063bfb91ac37
Stephan Zlatarev <stephan.zlatarev@gmail.com>
2026-08-06 08:31:59 +0300

    Extract target-matrix.js within combat skill

---
+++ skill/starcraft/combat/target-matrix.js
@@ -0,0 +1,138 @@
+
+/**
+- Filter targets in range of ranged warriors
+- Sort targets by how dangareous it is - damage divided by armor
+- For each target:
+  - Filter warriors in range
+  - Sort warrior by other targets in range
+  - Assign targets, until target health is less than assigned warrior damage
+**/
+export default class TargetMatrix {
+
+  allTargets = [];
+  primaryTargets = [];
+  targetsInRange = [];
+  targetToWarriors = new Map();
+  warriorToDamage = new Map();
+  warriorToTargets = new Map();
+  warriorToFightJob = new Map();
+  assignedWarriors = new Set();
+
+  constructor(battle) {
+    const groundWarriors = new Set();
+    const airRangeWarriors = new Set();
+    const groundRangeWarriors = new Set();
+
+    for (const fighter of battle.fighters) {
+      const warrior = fighter.assignee;
+      if (!warrior || !warrior.isAlive) continue;
+      if (warrior.type.rangeAir > 1) airRangeWarriors.add(warrior);
+      if (warrior.type.rangeGround > 1) groundRangeWarriors.add(warrior);
+      groundWarriors.add(warrior);
+      this.warriorToFightJob.set(warrior, fighter);
+    }
+
+    for (const sector of battle.sectors) {
+      for (const threat of sector.threats) {
+        if (!threat.isValidShootingTarget()) continue;
+
+        this.allTargets.push(threat);
+
+        if (!threat.type.isWarrior) continue;
+
+        // Treat the enemy warriors that are in the battle zone and those that have range over my warriors as primary targets
+        const isAnAttackerInBattleFront = threat.type.damageGround && (threat.zone === battle.front);
+        if (isAnAttackerInBattleFront || isEnemyWarriorAbleToAttack(battle.front, threat, groundWarriors)) {
+          this.primaryTargets.push(threat);
+        }
+      }
+    }
+
+    for (const target of this.primaryTargets) {
+      const warriors = target.body.isFlying ? airRangeWarriors : groundRangeWarriors;
+
+      for (const warrior of warriors) {
+        const range = target.body.isFlying ? warrior.type.rangeAir : warrior.type.rangeGround;
+        const squareRange = range * range;
+
+        if (calculateSquareDistance(warrior.body, target.body) <= squareRange) {
+          let inRangeWarriors = this.targetToWarriors.get(target);
+          let inRangeTargets = this.warriorToTargets.get(warrior);
+
+          if (!inRangeWarriors) {
+            inRangeWarriors = new Set();
+            this.targetToWarriors.set(target, inRangeWarriors);
+          }
+
+          if (!inRangeTargets) {
+            inRangeTargets = new Set();
+            this.warriorToDamage.set(warrior, 0);
+            this.warriorToTargets.set(warrior, inRangeTargets);
+          }
+
+          inRangeWarriors.add(warrior);
+          inRangeTargets.add(target);
+          this.warriorToDamage.set(warrior, this.warriorToDamage.get(warrior) + target.type.damageGround);
+        }
+      }
+    }
+
+    // Orders target by their damage per second
+    this.targetsInRange = [...this.targetToWarriors.keys()].sort(orderTargets);
+  }
+
+  isWarriorAssigned(warrior) {
+    return this.assignedWarriors.has(warrior);
+  }
+
+  // Lists the warriors in range of the given target. Orders the warriors with the least other targets first.
+  listWarriorsForTargetByInRangeOtherTargets(target) {
+    const warriors = this.targetToWarriors.get(target);
+
+    if (warriors.size) {
+      return [...warriors].sort((a, b) => (this.warriorToDamage.get(a) - this.warriorToDamage.get(b)));
+    } else {
+      return [];
+    }
+  }
+
+  assign(warrior, target) {
+    const fighter = this.warriorToFightJob.get(warrior);
+    fighter.target = target;
+
+    this.assignedWarriors.add(warrior);
+
+    for (const [ally, targets] of this.warriorToTargets) {
+      if (targets.has(target)) {
+        targets.delete(target);
+        this.warriorToDamage.set(ally, this.warriorToDamage.get(ally) - target.type.damageGround);
+      }
+    }
+
+    for (const warriors of this.targetToWarriors.values()) {
+      if (warriors.has(warrior)) {
+        warriors.delete(warrior);
+      }
+    }
+  }
+}
+
+function isEnemyWarriorAbleToAttack(zone, enemy, groundWarriors) {
+  if (enemy.type.rangeGround < 1) return false;
+
+  const groundRange = enemy.type.rangeGround + enemy.body.r + 1; // Use 1 for body radius of my warriors to reduce calculations
+  const squareGroundRange = groundRange * groundRange;
+
+  for (const warrior of groundWarriors) {
+    if (warrior.zone !== zone) continue;
+    if (calculateSquareDistance(enemy.body, warrior.body) <= squareGroundRange) return true;
+  }
+}
+
+function orderTargets(a, b) {
+  return (b.type.damageGround / b.armor.total) - (a.type.damageGround / a.armor.total);
+}
+
+function calculateSquareDistance(a, b) {
+  return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+}
